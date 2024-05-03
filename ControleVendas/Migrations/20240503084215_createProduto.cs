dc47cea1240967ce7810599e3475300209b37ef6
jaquesilva <jaqueline.natario@gmail.com>
2024-05-03 05:47:28 -0300

    Feat: Criação da model 'Produtos'.

---
+++ ControleVendas/Migrations/20240503084215_createProduto.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ControleVendas.Migrations
+{
+    /// <inheritdoc />
+    public partial class createProduto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produto",
+                columns: table => new
+                {
+                    ProdutoID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeProduto = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CustoUnitario = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PrecoVenda = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Fornecedor = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produto", x => x.ProdutoID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produto");
+        }
+    }
+}
--- ControleVendas/Models/Produto.cs
+++ ControleVendas/Models/Produto.cs
@@ -1,7 +1,24 @@
-﻿namespace ControleVendas.Models
+﻿using System.Collections.Specialized;
+using System.ComponentModel.DataAnnotations;
+
+namespace ControleVendas.Models
 {
     public class Produto
     {
-        public int MyProperty { get; set; }
+        public int? ProdutoID { get; set; }
+
+        [Display(Name = "Produto")]
+        public string? NomeProduto{ get; set; }
+
+        [Display(Name = "Descrição")]
+        public string? Descricao { get; set; }
+
+        [Display(Name = "Custo Unitário")]
+        public decimal  CustoUnitario { get; set; }
+
+        [Display(Name = "Preço de Venda")]
+        public decimal PrecoVenda { get; set; }
+
+        public string? Fornecedor { get; set; }
     }
 }
